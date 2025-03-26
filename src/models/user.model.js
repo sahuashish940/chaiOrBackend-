04650768d0d1798925e3d051618c93e2749cb738
Ashish sahu <sahuashish940@gmail.com>
2025-03-26 18:40:10 +0530

    update

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
